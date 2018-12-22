bbf4081212a22b6d2621954f635463eb42d93ec6
Stefano Carluccio <stefano.carluccio@student.reutlingen-university.de>
2018-12-22 09:16:36 +0100

    Basic style-variables added + RecordingComponent started

---
+++ audiolines/src/components/recording/recording.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+/**
+ * Generated class for the RecordingComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: "recording",
+  templateUrl: "recording.html"
+})
+export class RecordingComponent {
+  onClick() {}
+}
