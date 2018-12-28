a0915826bae7bae6cd29d650398ef10edfd1a54f
alinaaldea <43847920+alinaaldea@users.noreply.github.com>
2018-12-28 18:35:30 +0100

    export view created

---
+++ audiolines/src/components/components.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { ExportViewComponent } from './export-view/export-view';
+@NgModule({
+	declarations: [ExportViewComponent],
+	imports: [],
+	exports: [ExportViewComponent]
+})
+export class ComponentsModule {}
