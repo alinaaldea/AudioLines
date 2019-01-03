b02fb10cb7d7eb448a8197e7fa60d84ab895762c
Stefano Carluccio <stefano.carluccio@student.reutlingen-university.de>
2019-01-03 12:34:38 +0100

    wavecoloration works as intended

--- audiolines/src/components/waves-list/waves-list.ts
+++ audiolines/src/components/waves-list/waves-list.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, AfterViewChecked } from "@angular/core";
 
 import { StateManagerProvider } from "../../providers/state-manager/state-manager";
 
@@ -6,25 +6,25 @@ import { StateManagerProvider } from "../../providers/state-manager/state-manage
   selector: "waves-list",
   templateUrl: "waves-list.html"
 })
-export class WavesListComponent {
+export class WavesListComponent implements AfterViewChecked {
   waveTrackColors: { waveColor: string; progressColor: string }[] = [
-    { waveColor: "#EB0202", progressColor: "#690000" }, //red-ish
-    { waveColor: "#48CC01", progressColor: "#205C00" }, //green-ish
+    { waveColor: "#EB0202", progressColor: "#690000" }, // red-ish
+    { waveColor: "#48CC01", progressColor: "#205C00" }, // green-ish
     { waveColor: "#0D4F99", progressColor: "#052345" }, // blue-ish
     { waveColor: "pink", progressColor: "purple" },
     { waveColor: "#EBAC02", progressColor: "#694D00" } // yellow-ish
   ];
 
+  nextColor: { waveColor: string; progressColor: string };
+
+  ngAfterViewChecked(): void {
+    this.nextColor = this.getNextColor();
+  }
+
   constructor(public stateManager: StateManagerProvider) {}
 
   getNextColor(): { waveColor: string; progressColor: string } {
-    /**
-     * TODO: ARRAY-BUG
-     * somehow loops through all the elements
-     * when it should just take the first one and push it last
-     */
     let color = this.waveTrackColors.shift();
-    console.log(color);
     this.waveTrackColors.push(color);
     return color;
   }
