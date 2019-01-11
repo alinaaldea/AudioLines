bee2a65ff256477cee5a010d1012cb28fba6c230
Stefano Carluccio <stefano.carluccio@student.reutlingen-university.de>
2019-01-11 17:35:37 +0100

    TODOs Updated

--- audiolines/src/components/export-view/export-view.ts
+++ audiolines/src/components/export-view/export-view.ts
@@ -7,13 +7,6 @@ import { StateManagerProvider } from "../../providers/state-manager/state-manage
 
 declare var lamejs: any;
 
-/**
- * TODO: Export-Status Progress (OPTIONAL)
- *
- * for the UX it would be nice too see some kind of progressbar or spinner for the exporting
- * right now its just waiting until its done after about 10-15 seconds
- */
-
 @Component({
   selector: "export-view",
   templateUrl: "export-view.html"
