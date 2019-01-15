f01cd1cefea11fcc4b6703f6bbb5e7e0be81e9f2
Stefano Carluccio <stefano.carluccio@student.reutlingen-university.de>
2019-01-15 15:44:13 +0100

    Onboarding works -> needs to be styled

--- audiolines/src/app/app.component.ts
+++ audiolines/src/app/app.component.ts
@@ -2,36 +2,53 @@ import { Component } from "@angular/core";
 import { Platform } from "ionic-angular";
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
-import {Storage} from '@ionic/storage';
-import {HomePage} from "../pages/home/home";
-import {OnboardingComponent} from "../components/onboarding/onboarding";
-
+import { Storage } from "@ionic/storage";
+import { HomePage } from "../pages/home/home";
+import { OnboardingComponent } from "../components/onboarding/onboarding";
 
 @Component({
   templateUrl: "app.html"
 })
 export class MyApp {
-  rootPage: any = OnboardingComponent;
+  rootPage: any;
 
   constructor(
     platform: Platform,
     statusBar: StatusBar,
     splashScreen: SplashScreen,
-    public storage: Storage
+    private storage: Storage
   ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
-      /*if(this.storage.get('showOnboarding') == undefined) {
-        alert("No skip!");
-        this.rootPage = OnboardingComponent;
-        this.storage.set('showOnboarding', false);
-      } else {
-        alert("skip!");
-        this.rootPage = HomePage;
-      }*/
+
       splashScreen.hide();
+      //check if Onboarding should be displayed
+      this.storage
+        .ready()
+        .then(() => {
+          this.storage
+            .get("showOnboarding")
+            .then(valueOfKey => {
+              if (valueOfKey == null || valueOfKey == true) {
+                this.storage
+                  .set("showOnboarding", true)
+                  .then(() => {
+                    this.rootPage = OnboardingComponent;
+                  })
+                  .catch();
+              } else if (valueOfKey == false) {
+                this.rootPage = HomePage;
+              }
+            })
+            .catch(e => {
+              console.log(e);
+            });
+        })
+        .catch(e => {
+          console.log(e);
+        });
     });
   }
 }
--- audiolines/src/components/onboarding/onboarding.ts
+++ audiolines/src/components/onboarding/onboarding.ts
@@ -1,32 +1,37 @@
-import { Component } from '@angular/core';
-import {NavController} from "ionic-angular";
-import {HomePage} from "../../pages/home/home";
-import { ViewChild } from '@angular/core';
-import { Slides } from 'ionic-angular';
-
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { HomePage } from "../../pages/home/home";
+import { ViewChild } from "@angular/core";
+import { Slides } from "ionic-angular";
+import { Storage } from "@ionic/storage";
 
 @Component({
-  selector: 'onboarding',
-  templateUrl: 'onboarding.html'
+  selector: "onboarding",
+  templateUrl: "onboarding.html"
 })
 export class OnboardingComponent {
-
   @ViewChild(Slides) slides: Slides;
   public skipMsg: string = "Skip";
 
-  constructor(public navCtrl: NavController) {
-  }
+  constructor(public navCtrl: NavController, private storage: Storage) {}
 
-  ngOnInit(){
-  }
+  ngOnInit() {}
 
   public skip() {
-    this.navCtrl.push(HomePage);
+    this.storage
+      .set("showOnboarding", false)
+      .then(() => {
+        this.storage.get("showOnboarding").then(value => {
+          console.log(value);
+          this.navCtrl.push(HomePage);
+        });
+      })
+      .catch(e => {
+        console.log(e);
+      });
   }
 
   slideChanged() {
-    if (this.slides.isEnd())
-      this.skipMsg = "Alright, I got it";
+    if (this.slides.isEnd()) this.skipMsg = "Alright, I got it";
   }
-
 }
